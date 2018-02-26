259584d2640aba5fa36bab09e6101c364d1eba56
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-02-26 13:40:53 -0600

    excercise3

---
+++ scripts/excercise3.js
@@ -0,0 +1,42 @@
+function lvl3exercise1() {
+    // Create a "hello" and a "world", return the concatenation of the two
+    
+    var word1 = "hello"
+    var word2 = "world"
+
+    return word1 + word2
+
+}
+
+function lvl3exercise2() {
+    // Create a "hello" and a 12, return the concatenation of the two
+    var key = "hello"
+    var number = 12
+    return key + number
+    console.log(key + number)
+
+}
+
+function lvl3exercise3() {
+    // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+    var number = 12
+    number+='l'
+
+    return number
+
+
+
+}
+
+function lvl3exercise4() {
+	// Create a "hello world!" string. Return the length of the string
+
+}
+
+function lvl3exercise5() {
+	// Create a "hello world!" string. Return the index of the word "world".
+	
+}
+
+var x=lvl3exercise3()
+console.log(x*2)
