a3dd0777cef9e80441e62cc44226ff6ee169b3bf
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-02-26 11:57:42 -0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,74 @@
+
+function lvl1exercise1() {
+    // Declare a variable without instantiating it and return it!
+    var number;
+
+    return number;
+
+}
+
+function lvl1exercise2() {
+    // Declare and instantiate a number and return it
+    var number = 5;
+    return number;
+
+}
+
+function lvl1exercise3() {
+    // Declare and instantiate a floating point number that isn't a whole number and return it
+    
+    var number = 5.1;
+    return number;
+
+}
+
+function lvl1exercise4() {
+    // Declare and instantiate a string "Hello World!" and return it
+    var string = "Hello World";
+
+    return string;
+
+}
+
+function lvl1exercise5() {
+    // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+    var arr = ["Hello World!", 4 ];
+
+
+    return arr;
+}
+
+function lvl1exercise6() {
+	// Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+    var key1 = "Hello World!";
+    var key2 = 4
+
+    return key1 + key2
+}
+
+function lvl1exercise7() {
+    // Declare and instantiate a boolean value 'false' and return it
+    var answer = false
+
+    return answer
+
+}
+
+
+var result = lvl1exercise7();
+console.log( result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
